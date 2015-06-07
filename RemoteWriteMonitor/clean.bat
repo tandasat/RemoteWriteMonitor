3b587fe00efa5e9aea35a392a8a2891e8eb74e26
tandasat <tanda.sat@gmail.com>
2015-06-07 16:23:33 -0700

    Use default certificate

--- RemoteWriteMonitor/clean.bat
+++ RemoteWriteMonitor/clean.bat
@@ -15,5 +15,10 @@ rmdir /s /q RemoteWriteMonitor\Win7Release
 rmdir /s /q RemoteWriteMonitor\Win8.1Debug
 rmdir /s /q RemoteWriteMonitor\Win8.1Release
 rmdir /s /q RemoteWriteMonitor\x64
+rmdir /s /q "RemoteWriteMonitor Package\Win7Debug"
+rmdir /s /q "RemoteWriteMonitor Package\Win7Release"
+rmdir /s /q "RemoteWriteMonitor Package\Win8.1Debug"
+rmdir /s /q "RemoteWriteMonitor Package\Win8.1Release"
+rmdir /s /q "RemoteWriteMonitor Package\x64"
 del /s *.aps
 pause
